a40ef3b1d0fbdfae29b294ba59de2e2bd1aa0117
Junyoung Lee <junyoung.plum@gmail.com>
2016-10-11 05:51:33 +0900

    Fixed range of numbers

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ function generate() {
   var numbers = [];
 
   for(var i=0; i<6; i++) {
-    numbers.push(Math.floor(Math.random() * 45));
+    numbers.push(Math.floor(Math.random() * 45) + 1);
   }
 
   return numbers;
