80349b0553e4846890aae5e3e9000f0e6e1ab750
Junyoung Lee <junyoung.plum@gmail.com>
2016-10-11 06:03:28 +0900

    Changed the function comment(master)

--- js/app.js
+++ js/app.js
@@ -2,9 +2,23 @@ function generate() {
   var numbers = [];
 
   for(var i=0; i<6; i++) {
-    numbers.push(Math.floor(Math.random() * 45) + 1);
+    var number = Math.floor(Math.random() * 45) + 1;
+
+    if(contains(numbers, number)) {
+      i--;
+    } else {
+      numbers.push(number);
+    }
   }
 
   return numbers;
+}
+
+/* check a duplicated number in array */
+function contains(numbers, number) {
+  for(var i=0; i<numbers.length; i++) {
+    if(numbers[i] == number) return true;
+  }
+  return false;
 }
 
